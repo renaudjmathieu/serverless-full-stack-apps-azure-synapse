00cd911b1cf5e476412193e31dc37424e9224d5e
Renaud Mathieu <renaudjmathieu@gmail.com>
2022-11-22 23:14:48 -0500

    Step 3 : Ingest data from Azure Blob Storage with a serverless Function

--- LocalFunctionProj/function_app.py
+++ LocalFunctionProj/function_app.py
@@ -1,10 +1,23 @@
-import azure.functions as func
 import logging
 import os
+from io import StringIO
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
+
+import azure.functions as func
 from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient
 
 app = func.FunctionApp()
+
+def return_blob_files(container_client, arg_date, std_date_format):
+    start_date = datetime.strptime(arg_date, std_date_format).date() - timedelta(days=1)
+
+    blob_files = [blob for blob in container_client.list_blobs() if blob.creation_time.date() >= start_date]
+
+    return blob_files
 
 @app.function_name(name="HttpTrigger1")
 @app.route(route="hello") # HTTP Trigger
@@ -12,6 +25,9 @@ def test_function(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     # Parameters/Configurations
+    arg_date = '2014-07-01'
+    std_date_format = '%Y-%m-%d'
+
     abs_acct_name='blobstoragereno'
     abs_acct_url=f'https://{abs_acct_name}.blob.core.windows.net/'
     abs_container_name='demo-cloudetl-data'
@@ -27,12 +43,28 @@ def test_function(req: func.HttpRequest) -> func.HttpResponse:
         secret_client = SecretClient(vault_url=key_vault_Uri, credential= az_credential)
         access_key_secret = secret_client.get_secret(blob_secret_name)
 
+        # Initialize Azure Service SDK Clients
+        abs_service_client = BlobServiceClient(
+            account_url = abs_acct_url,
+            credential = az_credential
+        )
+
+        abs_container_client = abs_service_client.get_container_client(container=abs_container_name)
+
+        # Run ETL Application
+        process_file_list = return_blob_files(
+            container_client = abs_container_client,
+            arg_date = arg_date,
+            std_date_format = std_date_format
+        )
+
     except Exception as e:
         logging.info(e)
+
         return func.HttpResponse(
                 f"!! This HTTP triggered function executed unsuccessfully. \n\t {e} ",
                 status_code=200
-            )
+        )
 
     return func.HttpResponse("This HTTP triggered function executed successfully.")
 
