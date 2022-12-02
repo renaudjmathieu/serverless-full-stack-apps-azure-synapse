3de5181fdb675ad1ecd4ea635ea79a6c997458fb
Renaud Mathieu <renaudjmathieu@gmail.com>
2022-12-02 06:06:56 +0000

    Get data and store

--- LocalFunctionProj/function_app.py
+++ LocalFunctionProj/function_app.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 import pandas as pd
@@ -11,6 +12,15 @@ from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient, StandardBlobTier
 from azure.storage.filedatalake import DataLakeServiceClient
+
+from shared.azure_credential import (
+    get_azure_default_credential,
+    get_azure_key_credential,
+)
+from shared.bing_search import get_news
+from shared.blob_storage import upload_to_blob
+from shared.hash import get_random_hash
+from shared.key_vault_secret import get_key_vault_secret
 
 def return_blob_files(container_client, arg_date, std_date_format):
     start_date = datetime.strptime(arg_date, std_date_format).date() - timedelta(days=1)
@@ -204,6 +214,51 @@ def demo_relational_data_cloudetl(req: func.HttpRequest) -> func.HttpResponse:
 
 @app.function_name(name="ApiSearch")
 @app.route(route="search") # HTTP Trigger
-def test_function(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-    return func.HttpResponse("ApiSearch function processed a request!!!")
+def api_search(req: func.HttpRequest) -> func.HttpResponse:
+    # Get the query parameters
+    search_term = req.params.get("search_term", "Quantum Computing")
+    count = req.params.get("count", 10)
+
+    # Get environment variables
+    key_vault_resource_name = os.environ["KEY_VAULT_RESOURCE_NAME"]
+    bing_secret_name = os.environ["KEY_VAULT_SECRET_NAME"]
+    bing_news_search_url = os.environ["BING_SEARCH_URL"]
+    blob_account_name = os.environ.get("BLOB_STORAGE_RESOURCE_NAME")
+    blob_container_name = os.environ["BLOB_STORAGE_CONTAINER_NAME"]
+
+    # Get authentication to Key Vault with environment variables
+    azure_default_credential = get_azure_default_credential()
+
+    # Get the secret from Key Vault
+    bing_key = get_key_vault_secret(
+        azure_default_credential, key_vault_resource_name, bing_secret_name
+    )
+
+    # Get authentication to Bing Search with Key
+    azure_key_credential = get_azure_key_credential(bing_key)
+
+    # Clean up file name
+    random_hash = get_random_hash()
+    filename = f"search_results_{search_term}_{random_hash}.json".replace(" ", "_").replace(
+        "-", "_"
+    )
+
+    # Get the search results
+    news_search_results = get_news(azure_key_credential, bing_news_search_url, search_term, count)
+
+    # Convert the result to JSON and save it to Azure Blob Storage
+    if news_search_results.value:
+        news_item_count = len(news_search_results.value)
+        logging.info("news item count: %d", news_item_count)
+        json_items = json.dumps([news.as_dict() for news in news_search_results.value])
+
+        blob_url = upload_to_blob(
+            azure_default_credential,
+            blob_account_name,
+            blob_container_name,
+            filename,
+            json_items,
+        )
+        logging.info("news uploaded: %s", blob_url)
+
+    return filename
---
+++ LocalFunctionProj/shared/hash.py
@@ -0,0 +1,7 @@
+# ./shared/hash.py
+import random
+import string
+
+def get_random_hash():
+    random_hash = "".join(random.sample(string.ascii_letters + string.digits, 15))
+    return random_hash
